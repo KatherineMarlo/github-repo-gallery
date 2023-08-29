b4f6353d976ab6135a8e47bea96578ff9e30d482
katherine_m <kcreech1221@gmail.com>
2023-08-29 17:13:52 -0400

    Udated individual repo api

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 ///*****GLOBAL STYLES********//
 //Overview Div- Profile information//
 const overviewDiv = document.querySelector(".overview");
-//Display of Repos//
+//Display of Repos List//
 const repoDisplay = document.querySelector(".repo-list");
+//sectiion of repo//
+const sectionRepo = document.querySelector(".repos");
+//Individual Repo Data//
+const singleRepoData = document.querySelector(".repo-data");
 //gitHub username//
 const username = "KatherineMarlo";
 
 const githubInfo = async function () {
-    const profileInfo = await fetch (`https://api.github.com/users/${username}`);
-    const data = await profileInfo.json();
-    console.log(data);
-    displayUserInfo(data);
+  const profileInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await profileInfo.json();
+  console.log(data);
+  displayUserInfo(data);
 };
 
 githubInfo();
 
 //use the data.(Whatevervalue) to target the data variable and the json info then the info that you are trying to target//
 const displayUserInfo = function (data) {
-    const userDiv = document.createElement("div");
-    userDiv.innerHTML = `
+  const userDiv = document.createElement("div");
+  userDiv.innerHTML = `
     <figure>
       <img alt="user avatar" src=${data.avatar_url} />
     </figure>
@@ -33,12 +37,13 @@ const displayUserInfo = function (data) {
   fetchRepo();
 };
 
-const fetchRepo = async function() {
+const fetchRepo = async function () {
   const repoSite = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   const repoData = await repoSite.json();
   displayRepo(repoData);
 };
 
+//In the first case, the repo.name variable is used to access the name property of the repo object. This object is an array of repository objects that is passed to the displayRepo() function. The name property of the repo object contains the name of the repository.//
 const displayRepo = function (repos) {
   for (const repo of repos) {
     const listItem = document.createElement("li");
@@ -48,3 +53,43 @@ const displayRepo = function (repos) {
   }
 };
 
+repoDisplay.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    //console.log(repoName);
+    specificRepo(repoName);
+  }
+});
+
+//In the second case, the repoName variable is used to store the inner text of the element that was clicked on. The inner text of the element is the name of the repository.
+//The reason why we use different ways to target the repository name in these two cases is because the data is stored in different ways. In the first case, the data is stored in an object, and we can access the property that we need using dot notation. In the second case, the data is stored in the inner text of an element, and we need to use the innerText property to access it.//
+const specificRepo = async function (repoName) {
+  const individualRepo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await individualRepo.json();
+  console.log(repoInfo);
+
+  const fetchLanguages = await fetch(`https://api.github.com/repos/${username}/${repoName}/languages`);
+  const languageData = await fetchLanguages.json();
+  console.log(languageData);
+  const languages = [];
+  for (const language in languageData) {
+    languages.push(language);
+  }
+  displaySpecificRepo(repoInfo, languages);
+};
+
+
+const displaySpecificRepo = function (repoInfo, languages) {
+  singleRepoData.innerHTML = "";
+  const div = document.createElement("div");
+  div.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+  <p>Description: ${repoInfo.description}</p>
+  <p>Default Branch: ${repoInfo.default_branch}</p>
+  <p>Languages: ${languages.join(", ")}</p>
+  <a class="visit" href="${repoInfo.url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+  `;
+  console.log(languages);
+  singleRepoData.append(div);
+  singleRepoData.classList.remove("hide");
+  sectionRepo.classList.add("hide");
+};
